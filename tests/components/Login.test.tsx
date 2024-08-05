daa1bea6d846d51f92632ce8d1be6235544f336b
Ogheneyoma <Lawrenceyoma@gmail.com>
2024-08-05 15:49:15 +0100

    test: add tests for Register Page

--- tests/components/Login.test.tsx
+++ tests/components/Login.test.tsx
@@ -4,96 +4,98 @@ import { BrowserRouter } from 'react-router-dom'
 
 // Test for Login Component following TDD approach(how it would be when creating the component)
 
-test("Email and Password input fields should be empty on page load", () => {
-  render(
-    <BrowserRouter>
-      <Login />
-    </BrowserRouter>
-  )
+describe("Login Component", () => {
+  test("Email and Password input fields should be empty on page load", () => {
+    render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
 
-  const emailInputEl = screen.getByPlaceholderText(/email/i);
-  const passwordInputEl = screen.getByPlaceholderText(/password/i);
+    const emailInputEl = screen.getByPlaceholderText(/email/i);
+    const passwordInputEl = screen.getByPlaceholderText(/password/i);
 
-  expect(emailInputEl).toHaveValue("");
-  expect(passwordInputEl).toHaveValue("");
-})
+    expect(emailInputEl).toHaveValue("");
+    expect(passwordInputEl).toHaveValue("");
+  });
 
-test("Login Button should be disabled until user enters value in input elements", () => {
-  render(
-    <BrowserRouter>
-      <Login />
-    </BrowserRouter>
-  );
+  test("Login Button should be disabled until user enters value in input elements", () => {
+    render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
 
-  const buttonEl = screen.getByRole("button");
-  expect(buttonEl).toBeDisabled();
+    const buttonEl = screen.getByRole("button");
+    expect(buttonEl).toBeDisabled();
+  });
+
+  test("Email input should change value when user types in it", () => {
+    render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
+
+    const emailInputEl = screen.getByPlaceholderText(/email/i);
+    const testValue = "test";
+
+    fireEvent.change(emailInputEl, { target: { value: testValue } });
+    expect(emailInputEl).toHaveValue(testValue);
+  });
+
+  test("Password input should change value when user types in it", () => {
+    render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
+
+    const passwordInputEl = screen.getByPlaceholderText(/password/i);
+    const passwordValue = "password";
+
+    fireEvent.change(passwordInputEl, { target: { value: passwordValue } });
+    expect(passwordInputEl).toHaveValue(passwordValue);
+  });
+
+  test("Button should not be disabled when user types in email and password input elements", () => {
+    render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
+
+    const emailInputEl = screen.getByPlaceholderText(/email/i);
+    const passwordInputEl = screen.getByPlaceholderText(/password/i);
+    const buttonEl = screen.getByRole("button");
+
+    const testValue = "test";
+
+    fireEvent.change(emailInputEl, { target: { value: testValue } });
+    fireEvent.change(passwordInputEl, { target: { value: testValue } });
+
+    expect(buttonEl).not.toBeDisabled();
+  });
+
+  test("Loading text should be displayed when user clicks on the login button", () => {
+    render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
+
+    const emailInputEl = screen.getByPlaceholderText(/email/i);
+    const passwordInputEl = screen.getByPlaceholderText(/password/i);
+    const buttonEl = screen.getByRole("button");
+
+    const testValue = "test";
+
+    fireEvent.change(emailInputEl, { target: { value: testValue } });
+    fireEvent.change(passwordInputEl, { target: { value: testValue } });
+
+    // onClick test simulation
+    fireEvent.click(buttonEl);
+
+    expect(buttonEl).toHaveTextContent(/loading/i);
+  });
 });
-
-test("Email input should change value when user types in it", () => {
-  render(
-    <BrowserRouter>
-      <Login />
-    </BrowserRouter>
-  );
-
-  const emailInputEl = screen.getByPlaceholderText(/email/i);
-  const testValue = "test";
-
-  fireEvent.change(emailInputEl, { target: { value: testValue } });
-  expect(emailInputEl).toHaveValue(testValue);
-});
-
-test("Password input should change value when user types in it", () => {
-  render(
-    <BrowserRouter>
-      <Login />
-    </BrowserRouter>
-  )
-
-  const passwordInputEl = screen.getByPlaceholderText(/password/i);
-  const passwordValue = "password"
-
-  fireEvent.change(passwordInputEl, { target: { value: passwordValue } });
-  expect(passwordInputEl).toHaveValue(passwordValue);
-})
-
-test("Button should not be disabled when user types in email and password input elements", () => {
-  render(
-    <BrowserRouter>
-      <Login />
-    </BrowserRouter>
-  )
-
-  const emailInputEl = screen.getByPlaceholderText(/email/i);
-  const passwordInputEl = screen.getByPlaceholderText(/password/i);
-  const buttonEl = screen.getByRole("button");
-
-  const testValue = "test";
-
-  fireEvent.change(emailInputEl, { target: { value: testValue } });
-  fireEvent.change(passwordInputEl, { target: { value: testValue } });
-
-  expect(buttonEl).not.toBeDisabled();
-})
-
-test("Loading text should be displayed when user clicks on the login button", () => {
-  render(
-    <BrowserRouter>
-      <Login />
-    </BrowserRouter>
-  )
-
-  const emailInputEl = screen.getByPlaceholderText(/email/i);
-  const passwordInputEl = screen.getByPlaceholderText(/password/i);
-  const buttonEl = screen.getByRole("button");
-
-  const testValue = "test";
-
-  fireEvent.change(emailInputEl, { target: { value: testValue } });
-  fireEvent.change(passwordInputEl, { target: { value: testValue } });
-
-  // onClick test simulation
-  fireEvent.click(buttonEl);
-
-  expect(buttonEl).toHaveTextContent(/loading/i);
-})
--- tests/components/Register.test.tsx
+++ tests/components/Register.test.tsx
@@ -1,48 +1,120 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { Register } from '../../src/pages'
 import { BrowserRouter } from 'react-router-dom'
 
-test("All input fields should be empty on initial pageload", () => {
+describe("Register Component", () => {
+  test("All input fields should be empty on initial pageload", () => {
     render(
-        <BrowserRouter>
-            <Register />
-        </BrowserRouter>
-    )   
+      <BrowserRouter>
+        <Register />
+      </BrowserRouter>
+    );
 
-    const emailInputEl = screen.getByPlaceholderText(/email/i)
-    const passwordInputEl = screen.getByTestId("password")
-    const confirmPasswordInputEl = screen.getByTestId("confirm-password")
+    const emailInputEl = screen.getByPlaceholderText(/email/i);
+    const passwordInputEl = screen.getByTestId("password");
+    const confirmPasswordInputEl = screen.getByTestId("confirm-password");
     const studentNameInputEl = screen.getByPlaceholderText(/student name/i);
     const schoolNameInputEl = screen.getByPlaceholderText(/school name/i);
     const departmentInputEl = screen.getByPlaceholderText(/department/i);
 
-    expect(emailInputEl && passwordInputEl && confirmPasswordInputEl && studentNameInputEl && schoolNameInputEl && departmentInputEl).toHaveValue("")
-})
+    expect(
+      emailInputEl &&
+        passwordInputEl &&
+        confirmPasswordInputEl &&
+        studentNameInputEl &&
+        schoolNameInputEl &&
+        departmentInputEl
+    ).toHaveValue("");
+  });
 
-test("Register Button should be disabled until user enters value in input elements", () => {
+  test("Register Button should be disabled until user enters value in input elements", () => {
+    render(
+      <BrowserRouter>
+        <Register />
+      </BrowserRouter>
+    );
+
+    const buttonEl = screen.getByRole("button");
+    expect(buttonEl).toBeDisabled();
+  });
+
+  test("All inputs should change value when the user types in it", () => {
+    render(
+      <BrowserRouter>
+        <Register />
+      </BrowserRouter>
+    );
+
+    const emailInputEl = screen.getByPlaceholderText(/email/i);
+    const passwordInputEl = screen.getByTestId("password");
+    const confirmPasswordInputEl = screen.getByTestId("confirm-password");
+    const studentNameInputEl = screen.getByPlaceholderText(/student name/i);
+    const schoolNameInputEl = screen.getByPlaceholderText(/school name/i);
+    const departmentInputEl = screen.getByPlaceholderText(/department/i);
+
+    const testValue = "test";
+    fireEvent.change(emailInputEl, { target: { value: testValue } })
+    fireEvent.change(passwordInputEl, { target: { value: testValue } })
+    fireEvent.change(confirmPasswordInputEl, { target: { value: testValue } })
+    fireEvent.change(studentNameInputEl, { target: { value: testValue } })
+    fireEvent.change(schoolNameInputEl, { target: { value: testValue } })
+    fireEvent.change(departmentInputEl, { target: { value: testValue } })
+
+    expect(emailInputEl && passwordInputEl && confirmPasswordInputEl && studentNameInputEl && schoolNameInputEl && departmentInputEl).toHaveValue(testValue);
+    
+  });
+
+  test("Button should not be disabled when user types in all input elements", () => {
+    render(
+      <BrowserRouter>
+        <Register />
+      </BrowserRouter>
+    );
+
+    const emailInputEl = screen.getByPlaceholderText(/email/i);
+    const passwordInputEl = screen.getByTestId("password");
+    const confirmPasswordInputEl = screen.getByTestId("confirm-password");
+    const studentNameInputEl = screen.getByPlaceholderText(/student name/i);
+    const schoolNameInputEl = screen.getByPlaceholderText(/school name/i);
+    const departmentInputEl = screen.getByPlaceholderText(/department/i);
+
+    const testValue = "test";
+    fireEvent.change(emailInputEl, { target: { value: testValue } })
+    fireEvent.change(passwordInputEl, { target: { value: testValue } })
+    fireEvent.change(confirmPasswordInputEl, { target: { value: testValue } })
+    fireEvent.change(studentNameInputEl, { target: { value: testValue } })
+    fireEvent.change(schoolNameInputEl, { target: { value: testValue } })
+    fireEvent.change(departmentInputEl, { target: { value: testValue } })
+
+    const buttonEl = screen.getByRole("button");
+    expect(buttonEl).not.toBeDisabled();
+  })
+
+  test("Loading text should be displayed when user clicks on the login button", () => {
     render(
         <BrowserRouter>
             <Register />
         </BrowserRouter>
     )
 
-    const buttonEl = screen.getByRole("button");
-    expect(buttonEl).toBeDisabled();
-})
+    const emailInputEl = screen.getByPlaceholderText(/email/i);
+    const passwordInputEl = screen.getByTestId("password");
+    const confirmPasswordInputEl = screen.getByTestId("confirm-password");
+    const studentNameInputEl = screen.getByPlaceholderText(/student name/i);
+    const schoolNameInputEl = screen.getByPlaceholderText(/school name/i);
+    const departmentInputEl = screen.getByPlaceholderText(/department/i);
+    const buttonEl = screen.getByRole("button")
 
-// test("All inputs should change value when the user types in it", () => {
-//     render(  
-//         <BrowserRouter>
-//             <Register />
-//         </BrowserRouter>
-//     )
+    const testValue = "test";
+    fireEvent.change(emailInputEl, { target: { value: testValue } });
+    fireEvent.change(passwordInputEl, { target: { value: testValue } });
+    fireEvent.change(confirmPasswordInputEl, { target: { value: testValue } });
+    fireEvent.change(studentNameInputEl, { target: { value: testValue } });
+    fireEvent.change(schoolNameInputEl, { target: { value: testValue } });
+    fireEvent.change(departmentInputEl, { target: { value: testValue } });
 
-//     const emailInputEl = screen.getByPlaceholderText(/email/i)
-//     const passwordInputEl = screen.getByTestId("password")
-//     const confirmPasswordInputEl = screen.getByTestId("confirm-password")
-//     const studentNameInputEl = screen.getByPlaceholderText(/student name/i);
-//     const schoolNameInputEl = screen.getByPlaceholderText(/school name/i);
-//     const departmentInputEl = screen.getByPlaceholderText(/department/i);
+    fireEvent.click(buttonEl);
 
-
-// })
+    expect(buttonEl).toHaveTextContent(/loading/i);
+  });
+});
