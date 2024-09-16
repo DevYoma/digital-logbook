ccbcfdb432efb88c4520e6afe514d24afca0425b
Ogheneyoma <Lawrenceyoma@gmail.com>
2024-09-16 01:03:27 +0100

    chore: removed waitFor unused method

--- tests/components/Register.test.tsx
+++ tests/components/Register.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { Register } from '../../src/pages'
 import { BrowserRouter } from 'react-router-dom';
 
